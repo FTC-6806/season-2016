507b67e81becc7fdd6510c693d47e570a52b7157
Liam Marshall <archimedespi3141@gmail.com>
2015-11-06 19:23:39 -0600

    Add drive direction toggle for TheodoreTeleop

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TheodoreTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TheodoreTeleop.java
@@ -24,6 +24,10 @@ public class TheodoreTeleop extends OpMode { // declares a new opmode. IntelliJ 
 
   private JoystickScaler joystick_scaler;
 
+  private boolean driveDirectionButtonCurrentState;
+  private boolean driveDirectionButtonLastState;
+  private int drive_direction_scaling = 1;
+
   @Override
   public void init() {
     // set up the joystick scaler
@@ -45,27 +49,30 @@ public class TheodoreTeleop extends OpMode { // declares a new opmode. IntelliJ 
     // reverse one of the motors (because it's a tank drive)
     drive_right.setDirection(DcMotor.Direction.REVERSE);
 
-    // reverse one of the mechanism motors (because of the chain configuration on Alec's testing rig).
-    // Valid as of Oct 6, 2015. DON'T RUN THIS OPMODE WITHOUT CHECKING THIS! YOU'LL PROBABLY BREAK SOMETHING EXPENSIVE!
-    mechanism_b.setDirection(DcMotor.Direction.REVERSE);
-
     // set both of the drive motors to use a bit of PID
     drive_left.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
     drive_right.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
     // same with mechanism
-    mechanism_a.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-    mechanism_b.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+    mechanism_a.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+    mechanism_b.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
   }
 
   @Override
-  public void loop() { // this gets run again and again and again
-    // and every time around, we grab the stick value, negate them (because they come in with the wrong sign),
+  public void loop() {
+    driveDirectionButtonCurrentState = gamepad1.x || gamepad2.x;
+    if (driveDirectionButtonCurrentState == true && driveDirectionButtonCurrentState != driveDirectionButtonLastState) {
+      drive_direction_scaling *= -1;
+    }
+    // this gets run again and again and again
+    // and every time around, we grab the stick value,
     // and then set the power of the drivetrain motors.
-    drive_left.setPower(joystick_scaler.in(gamepad1.left_stick_y));
-    drive_right.setPower(joystick_scaler.in(gamepad1.right_stick_y));
+    drive_left.setPower(joystick_scaler.in(gamepad1.left_stick_y) * drive_direction_scaling);
+    drive_right.setPower(joystick_scaler.in(gamepad1.right_stick_y) * drive_direction_scaling);
 
     mechanism_a.setPower(joystick_scaler.in(gamepad2.left_stick_y));
     mechanism_b.setPower(joystick_scaler.in(gamepad2.left_stick_y));
+
+    telemetry.addData("01", "mech_a pf: " + mechanism_a.getPowerFloat() + "mech_b pf:" + mechanism_b.getPowerFloat());
   }
 }

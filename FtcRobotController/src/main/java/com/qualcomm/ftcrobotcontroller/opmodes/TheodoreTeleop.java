735b7e856a97a0c2ef59df0f8e61c4d0e06778b3
Liam Marshall <liam@cpan.org>
2015-11-21 15:25:31 -0600

    Clean up comments in TheodoreTeleop

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TheodoreTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TheodoreTeleop.java
@@ -6,13 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 
 import org.ratchetrobotics.algorithms.ai.ResponseCurve;
 import org.ratchetrobotics.algorithms.utilities.JoystickScaler;
-// everything before this line IntelliJ writes out for you automatically as you use stuff.
 
 /**
  * Created by liam on 9/19/2015.
  */
-public class TheodoreTeleop extends OpMode { // declares a new opmode. IntelliJ does this automatically for you.
-  // these are /kind of/ like the #pragmas in RobotC
+public class TheodoreTeleop extends OpMode {
   private DcMotorController drive_motor_controller;
   private DcMotorController mechanism_motor_controller;
 
@@ -33,16 +31,9 @@ public class TheodoreTeleop extends OpMode { // declares a new opmode. IntelliJ 
     // set up the joystick scaler
     joystick_scaler = new JoystickScaler();
 
-    // here we get a reference to the motor controller called "drive_controller" in the config file
-    drive_motor_controller = hardwareMap.dcMotorController.get("drive_controller");
-    // do the same for the auxiliary motor controller
-    mechanism_motor_controller = hardwareMap.dcMotorController.get("mechanism_controller");
-
-    // then let's get references to the motors "left_drive" and "right_drive"
     drive_left = hardwareMap.dcMotor.get("left_drive");
     drive_right = hardwareMap.dcMotor.get("right_drive");
 
-    // also get references to "mech_a" and "mech_b"
     mechanism_a = hardwareMap.dcMotor.get("mech_a");
     mechanism_b = hardwareMap.dcMotor.get("mech_b");
 
@@ -64,9 +55,7 @@ public class TheodoreTeleop extends OpMode { // declares a new opmode. IntelliJ 
     if (driveDirectionButtonCurrentState == true && driveDirectionButtonCurrentState != driveDirectionButtonLastState) {
       drive_direction_scaling *= -1;
     }
-    // this gets run again and again and again
-    // and every time around, we grab the stick value,
-    // and then set the power of the drivetrain motors.
+
     drive_left.setPower(joystick_scaler.in(gamepad1.left_stick_y) * drive_direction_scaling);
     drive_right.setPower(joystick_scaler.in(gamepad1.right_stick_y) * drive_direction_scaling);
 

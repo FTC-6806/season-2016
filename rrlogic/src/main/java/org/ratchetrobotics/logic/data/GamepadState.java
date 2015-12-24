ee946266102a7360e2968a5b140bb53053594fa1
Liam Marshall <liam@cpan.org>
2015-12-23 18:41:45 -0600

    Fully implement joystick state HAL

--- rrlogic/src/main/java/org/ratchetrobotics/logic/data/GamepadState.java
+++ rrlogic/src/main/java/org/ratchetrobotics/logic/data/GamepadState.java
@@ -7,15 +7,38 @@ import com.qualcomm.robotcore.hardware.Gamepad;
  */
 
 public class GamepadState {
-  public float stick_l_x, stick_l_y,
-               stick_r_x, stick_r_y;
+  public float left_stick_x, left_stick_y,
+    right_stick_x, right_stick_y;
   public boolean dpad_up, dpad_down, dpad_right, dpad_left;
   public boolean a, b, x, y;
   public boolean guide, start, back;
-  public boolean bumper_l, bumper_r;
-  public boolean stick_l_button, stick_r_button;
-  public boolean right_stick_button;
-  public float trigger_l, trigger_r;
-  public static void mirrorGamepadState(Gamepad gamepad) {
+  public boolean left_bumper, right_bumper;
+  public boolean left_stick_button, right_stick_button;
+  public float left_trigger, right_trigger;
+
+  /**
+   * Copy gamepad state from a Gamepad object into a new GamepadState
+   *
+   * @param gamepad a Qualcomm Gamepad class.
+   */
+  public GamepadState(Gamepad gamepad) {
+    this.left_stick_x = gamepad.left_stick_x;
+    this.left_stick_y = gamepad.left_stick_y;
+    this.right_stick_x = gamepad.right_stick_x;
+    this.right_stick_y = gamepad.right_stick_y;
+
+    this.dpad_up = gamepad.dpad_up; this.dpad_down = gamepad.dpad_down;
+    this.dpad_left = gamepad.dpad_left; this.dpad_right = gamepad.dpad_right;
+
+    this.a = gamepad.a; this.b = gamepad.b;
+    this.x = gamepad.x; this.y = gamepad.y;
+
+    this.guide = gamepad.guide; this.start = gamepad.start;
+    this.back = gamepad.back;
+
+    this.left_bumper = gamepad.left_bumper; this.right_bumper = gamepad.right_bumper;
+    this.left_stick_button = gamepad.left_stick_button; this.right_stick_button = gamepad.right_stick_button;
+
+    this.left_trigger = gamepad.left_trigger; this.right_trigger = gamepad.right_trigger;
   }
 }

0eaf38ab7de5db52c62707dc8b60f384e473e08f
Liam Marshall <liam@cpan.org>
2015-12-26 15:35:57 -0600

    Add motors to RAL, move to rrlogic

--- rrlogic/src/main/java/org/ratchetrobotics/logic/RobotAbstractionLayer.java
+++ rrlogic/src/main/java/org/ratchetrobotics/logic/RobotAbstractionLayer.java
@@ -1,13 +1,15 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
+package org.ratchetrobotics.logic;
 
 import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
-import org.ratchetrobotics.logic.ButtonEventTrigger;
-import org.ratchetrobotics.logic.GamepadWatcher;
-import org.ratchetrobotics.logic.Watcher;
+import org.ratchetrobotics.logic.events.ButtonEventTrigger;
+import org.ratchetrobotics.logic.events.GamepadWatcher;
+import org.ratchetrobotics.logic.events.Watcher;
 
 import java.util.List;
 
@@ -15,6 +17,9 @@ import java.util.List;
  * Created by liam on 12/14/15.
  */
 public class RobotAbstractionLayer extends OpMode {
+  protected DcMotor winchMotor;
+  protected Servo winchRatchetServo, winchAimingServo;
+
   protected List<Watcher> watchers;
 
   public RobotAbstractionLayer() {
@@ -25,6 +30,10 @@ public class RobotAbstractionLayer extends OpMode {
     watchers.add(new GamepadWatcher(gamepad, new ButtonEventTrigger(button, ButtonEventTrigger.EventType.PRESSED), callback));
   }
 
+  public void onButtonRelease(Gamepad gamepad, String button, GamepadWatcher.GamepadEventCallback callback) {
+    watchers.add(new GamepadWatcher(gamepad, new ButtonEventTrigger(button, ButtonEventTrigger.EventType.RELEASED), callback));
+  }
+
   @Override
   public void init() {}
 
@@ -32,6 +41,9 @@ public class RobotAbstractionLayer extends OpMode {
   public void loop() {}
 
   public void initializeHardware() {
+    winchMotor = hardwareMap.dcMotor.get("winchMotor");
+    winchRatchetServo = hardwareMap.servo.get("winchRatchetServo");
+    winchAimingServo = hardwareMap.servo.get("winchAimingServo");
   }
 
   public void tickWatchers() {
